597410adf5e3ae2af32ccc505cb124ef03803901
Jacob Althouse <jalthouse@cricut.com>
2018-07-03 13:00:40 -0600

    Added Tests

--- To_Do_List/To_Do_ListTests/To_Do_ListTests.m
+++ To_Do_List/To_Do_ListTests/To_Do_ListTests.m
@@ -7,33 +7,85 @@
 //
 
 #import <XCTest/XCTest.h>
-
+#import "ViewController.h"
 @interface To_Do_ListTests : XCTestCase
-
+@property (strong,nonatomic) ViewController* viewController;
+@property (strong, nonatomic) UIStoryboard* storyBoard;
 @end
 
 @implementation To_Do_ListTests
 
 - (void)setUp {
     [super setUp];
+    self.storyBoard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    self.viewController = [self.storyBoard instantiateViewControllerWithIdentifier:@"ToDoController"];
+    [self.viewController view];
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
 - (void)tearDown {
+    self.storyBoard = nil;
+    self.viewController = nil;
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
-- (void)testExample {
+- (void)testWhenTaskAddedToEmptyTable_ThenTableIsNoLongerNull {
+    Task *task = [[Task alloc] init];
+    task.taskId = 0;
+    task.taskName = @"test";
+    task.dueDate = [NSDate date];
+    [self.viewController createTask:(task)];
+    XCTAssertNotNil(self.viewController.taskTable, @"Table is not null");
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+-(void)testDeleteTask {
+    Task *task = [[Task alloc] init];
+    task.taskId = 0;
+    task.taskName = @"test";
+    task.dueDate = [NSDate date];
+    [self.viewController createTask:(task)];
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
+    [self.viewController tableView:[self.viewController taskTable] commitEditingStyle:UITableViewCellEditingStyleDelete forRowAtIndexPath:indexPath];
 }
 
+-(void)testDeleteWithMultipleTasks{
+    for(int i = 0; i < 4; i++){
+        Task *task = [[Task alloc] init];
+        task.taskId = i;
+        task.taskName = @"test";
+        task.dueDate = [NSDate date];
+        [self.viewController createTask:(task)];
+    }
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
+    [self.viewController tableView:[self.viewController taskTable] commitEditingStyle:UITableViewCellEditingStyleDelete forRowAtIndexPath:indexPath];
+    XCTAssertEqual(3, [self.viewController.taskTable numberOfRowsInSection:0]);
+}
+
+-(void) testEditTask{
+    for(int i = 0; i < 4; i++){
+        Task *task = [[Task alloc] init];
+        task.taskId = i;
+        task.taskName = @"test";
+        task.dueDate = [NSDate date];
+        [self.viewController createTask:(task)];
+    }
+    Task *editedTask = [[Task alloc] init];
+    editedTask.taskId = 0;
+    editedTask.taskName = @"editedTest";
+    editedTask.dueDate = [NSDate date];
+    [self.viewController createTask:(editedTask)];
+    XCTAssertEqual(4, [self.viewController.taskTable numberOfRowsInSection:0]);
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
+    UITableViewCell *cell =[self.viewController tableView:[self.viewController taskTable] cellForRowAtIndexPath:indexPath];
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setFormatterBehavior:NSDateFormatterBehavior10_4];
+    [formatter setDateStyle:NSDateFormatterShortStyle];
+    [formatter setTimeStyle:NSDateFormatterNoStyle];
+    NSString *result = [formatter stringFromDate: editedTask.dueDate];
+    NSString *expectedString = [NSString stringWithFormat:@"%@ | %@",editedTask.taskName, result];
+    XCTAssertEqualObjects(expectedString, cell.textLabel.text);
+}
 @end
